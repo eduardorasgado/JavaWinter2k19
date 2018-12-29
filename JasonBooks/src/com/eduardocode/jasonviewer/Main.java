5fdec21c71a800552c13940cb098fbccef74734e
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-12-28 22:40:04 -0600

    Jason Viewer project: Separating static methods for all actions in solution.
    MOdel.MOvie: constructor.

--- JasonBooks/src/com/eduardocode/jasonviewer/Main.java
+++ JasonBooks/src/com/eduardocode/jasonviewer/Main.java
@@ -1,47 +1,119 @@
 package com.eduardocode.jasonviewer;
 
+import com.eduardocode.jasonviewer.model.Movie;
+
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args)
     {
+        // instanciate an object
+        //Movie rambo = new Movie("Rambo");
+        //System.out.println(rambo.getTitle());
+
         int exit = 0;
         do {
-            System.out.println("[WELCOME TO JASON VIEWER]");
-            System.out.println("");
-            System.out.println("Selecciona la opcion a la que deseas accesar.");
-            System.out.println("1. Movies");
-            System.out.println("2. Series");
-            System.out.println("3. Books");
-            System.out.println("4. Magazines");
-            System.out.println("0. Exit");
+            printMenu();
+            takeDecision();
 
-            // Leer la respuesta del usuario
-            int opcion = 0;
-
-            switch (opcion) {
-                case 0:
-                    System.out.println("Elegiste Salir");
-                    break;
-                case 1:
-                    System.out.println("Elegiste movies");
-                    //showMovies();
-                    break;
-                case 2:
-                    System.out.println("Elegiste Series");
-                    //showSeries();
-                    break;
-                case 3:
-                    System.out.println("Elegiste Books");
-                    //showBooks();
-                    break;
-                case 4:
-                    System.out.println("Elegiste Magazines");
-                    //showMagazines();
-                    break;
-                default:
-                    System.out.println("Opcion inválida");
-                    break;
-            }
         } while(exit != 0);
+    }
+
+    private static void printMenu() {
+        System.out.println("[WELCOME TO JASON VIEWER]");
+        System.out.println("");
+        System.out.println("Selecciona la opcion a la que deseas accesar.");
+        System.out.println("1. Movies");
+        System.out.println("2. Series");
+        System.out.println("3. Books");
+        System.out.println("4. Magazines");
+        System.out.println("5. Report");
+        System.out.println("6. Report Today");
+        System.out.println("0. Exit");
+    }
+
+    private static void takeDecision()
+    {
+        // Leer la respuesta del usuario
+        int opcion = 1;
+
+        switch (opcion) {
+            case 0:
+                System.out.println("Elegiste Salir");
+                break;
+            case 1:
+                showMovies();
+                break;
+            case 2:
+                showSeries();
+                break;
+            case 3:
+                showBooks();
+                break;
+            case 4:
+                showMagazines();
+                break;
+            case 5:
+                makeReport();
+                break;
+            case 6:
+                Date today = new Date();
+                makeReport(today);
+                break;
+            default:
+                System.out.println("Opcion inválida");
+                break;
+        }
+    }
+
+    private static void showMovies()
+    {
+        System.out.println("Elegiste movies");
+        int exit = 1;
+        do {
+            System.out.println("::MOVIES::");
+            break;
+        } while (exit == 1);
+    }
+
+    private static void showSeries()
+    {
+        System.out.println("Elegiste Series");
+        int exit = 1;
+        do {
+            System.out.println("::SERIES::");
+            break;
+        } while (exit == 1);
+    }
+
+    private static void showBooks()
+    {
+        System.out.println("Elegiste Books");
+        int exit = 1;
+        do {
+            System.out.println("::BOOKS::");
+            break;
+        } while (exit == 1);
+    }
+
+    private static void showMagazines()
+    {
+        System.out.println("Elegiste Magazines");
+        int exit = 1;
+        do {
+            System.out.println("::MAGAZINES::");
+            break;
+        } while (exit == 1);
+    }
+
+    private static void makeReport()
+    {
+
+    }
+
+    private static void makeReport(Date date)
+    {
+
     }
 }
--- JasonBooks/src/com/eduardocode/jasonviewer/model/Movie.java
+++ JasonBooks/src/com/eduardocode/jasonviewer/model/Movie.java
@@ -2,6 +2,8 @@ package com.eduardocode.jasonviewer.model;
 
 public class Movie {
     // declaring attributes
+    // usually these data is private and they need
+    // setters and getters to be accesed
     int id;
     String title;
     String director;
@@ -9,4 +11,16 @@ public class Movie {
     boolean viewed;
     int timeViewed;
 
+    public Movie(String title)
+    {
+        // constructor
+        this.title = title;
+    }
+
+    // Getters and setters
+    public String getTitle(){
+        return title;
+    }
+
 }
+
