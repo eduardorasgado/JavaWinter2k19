05f3ca7b889e8610611ed052bdd6828684835841
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-01-03 04:10:15 -0600

    Jason Viewer project: Readable class creared and book extends from Readable.

--- JasonBooks/src/com/eduardocode/jasonviewer/model/Book.java
+++ JasonBooks/src/com/eduardocode/jasonviewer/model/Book.java
@@ -2,86 +2,20 @@ package com.eduardocode.jasonviewer.model;
 
 import java.util.Date;
 
-public class Book {
+public class Book extends Readable {
     // attributes
-    private int id;
-    private String title;
-    private Date editionDate;
-    private String genre;
-    private int duration;
-    private String editorial;
-    private String[] authors;
     private String isbn;
     // if book has been read
     private boolean read;
     // how much time do this book was read
     private int timeRead;
 
-    public Book(String title, Date editionDate, String editorial, String isbn) {
-        this.title = title;
-        this.editionDate = editionDate;
-        this.editorial = editorial;
+    public Book(String title, Date editionDate, String genre, String editorial, String isbn) {
+        super(title,editionDate,genre,editorial);
         this.isbn = isbn;
     }
 
     // GETTERS AND SETTERS
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Date getEditionDate() {
-        return editionDate;
-    }
-
-    public void setEditionDate(Date editionDate) {
-        this.editionDate = editionDate;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-
-    public String getEditorial() {
-        return editorial;
-    }
-
-    public void setEditorial(String editorial) {
-        this.editorial = editorial;
-    }
-
-    public String[] getAuthors() {
-        return authors;
-    }
-
-    public void setAuthors(String[] authors) {
-        this.authors = authors;
-    }
-
     public String getIsbn() {
         return isbn;
     }
---
+++ JasonBooks/src/com/eduardocode/jasonviewer/model/Readable.java
@@ -0,0 +1,63 @@
+package com.eduardocode.jasonviewer.model;
+
+import java.util.Date;
+
+public class Readable {
+    private int id;
+    private String title;
+    private Date editionDate;
+    private String genre;
+    private String editorial;
+    private String[] authors;
+
+    public Readable(String title, Date editionDate, String genre, String editorial) {
+        this.title = title;
+        this.editionDate = editionDate;
+        this.genre = genre;
+        this.editorial = editorial;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getEditionDate() {
+        return editionDate;
+    }
+
+    public void setEditionDate(Date editionDate) {
+        this.editionDate = editionDate;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public String[] getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String[] authors) {
+        this.authors = authors;
+    }
+}
